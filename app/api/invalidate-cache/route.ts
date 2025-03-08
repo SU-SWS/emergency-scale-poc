93ac07d05ba4d6a3402f88b275637c29902ac106
Shea McKinney <sheamck@stanford.edu>
2025-03-07 16:11:00 -0800

    Start 4

--- app/api/invalidate-cache/route.ts
+++ app/api/invalidate-cache/route.ts
@@ -1,8 +1,8 @@
-import { revalidateTag } from "next/cache"
-import { NextResponse } from "next/server"
+import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
 import { purgeCache } from "@netlify/functions";
 
-export async function POST() {
+export async function GET() {
   // Invalidate NextJS's the 'cards' cache tag
   revalidateTag("cards");
 
