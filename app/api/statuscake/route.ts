cc2497eb3ff10925c94050d623e90462c115f4fc
Shea McKinney <sheamck@stanford.edu>
2025-03-25 15:18:34 -0700

    0

--- app/api/statuscake/route.ts
+++ app/api/statuscake/route.ts
@@ -4,10 +4,10 @@ export async function POST(request: Request) {
   // Log request headers
   console.log('Request Headers:', [...request.headers.entries()]);
 
-  // Parse and log JSON payload
+  // Parse and log payload
   let payload;
   try {
-    payload = await request.json();
+    payload = await request.text();
     console.log('Request Payload:', payload);
   } catch (error) {
     console.error('Error parsing payload:', error);
